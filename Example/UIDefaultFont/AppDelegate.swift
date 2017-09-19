2b962eb88293b1376d3aba598a4d489ab77c94e4
Gaël Foppolo <me@gaelfoppolo.com>
2017-09-19 05:58:36 +0200

    fill example project

--- Example/UIDefaultFont/AppDelegate.swift
+++ Example/UIDefaultFont/AppDelegate.swift
@@ -18,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
+        UIFont.normalFontName = "Courier"
+        UIFont.boldFontName = "Noteworthy-Bold"
+        UIFont.italicFontName = "HelveticaNeue-ThinItalic"
+
         return true
     }
 
