f42f2c57225652ac38e6151fc47ea55c417f076e
Gaël Foppolo <me@gaelfoppolo.com>
2017-09-19 05:51:45 +0200

    add example project

---
+++ Example/Pods/Target Support Files/Pods-UIDefaultFont_Example/Pods-UIDefaultFont_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UIDefaultFont_Example : NSObject
+@end
+@implementation PodsDummy_Pods_UIDefaultFont_Example
+@end
---
+++ Example/UIDefaultFont/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UIDefaultFont
+//
+//  Created by Gaël Foppolo on 09/19/2017.
+//  Copyright (c) 2017 Gaël Foppolo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
