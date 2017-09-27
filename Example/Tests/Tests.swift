e927716e590e508c883eb40f5d6fd6bb2ee06338
Gaël Foppolo <me@gaelfoppolo.com>
2017-09-27 16:13:30 +0200

    add tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,29 +1,35 @@
-import UIKit
+//import UIKit
 import XCTest
-import UIDefaultFont
+@testable import UIDefaultFont
 
 class Tests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+
+    func testNormalFont() {
+
+        let fontName: String = "Georgia"
+        UIFont.normalFontName = fontName
+
+        XCTAssertTrue(UIFont.systemFont(ofSize: UIFont.systemFontSize).fontName == fontName, "\(fontName) font is not the default system font")
+
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+    func testBoldFont() {
+
+        let fontName: String = "Georgia-Bold"
+        UIFont.boldFontName = fontName
+
+        XCTAssertTrue(UIFont.boldSystemFont(ofSize: UIFont.systemFontSize).fontName == fontName, "\(fontName) font is not the default bold system font")
+
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testItalicFont() {
+
+        let fontName: String = "Georgia-Italic"
+        UIFont.boldFontName = fontName
+
+        XCTAssertTrue(UIFont.boldSystemFont(ofSize: UIFont.systemFontSize).fontName == fontName, "\(fontName) font is not the default italic system font")
+
     }
     
 }
